a51018cd5e33277f4dc13b98c668004aad409b3c
Mikołaj Nowacki <mikolajnowacki01@gmail.com>
2026-03-27 10:29:51 +0100

    345.py

---
+++ 345.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = 'aeiouAEIOU'
+        s = list(s)
+        i = 0
+        j = len(s)-1
+
+        while i < j:
+            if s[i] not in vowels:
+                i+=1
+            elif s[j] not in vowels:
+                j-=1
+            else:
+                s[i], s[j] = s[j], s[i]
+                i+=1
+                j-=1
+        
+        return ''.join(s)
